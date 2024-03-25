d422a5fb9ce6df9ef72f6399d5717a3e3f50eab2
Sharlles Andersonn <54425710+skvanderson@users.noreply.github.com>
2024-03-25 08:41:26 -0300

    Update Tabela_Hash.h
    Incluindo as funcionalidades na arquivo.h

--- Tabela_Hash.h
+++ Tabela_Hash.h
@@ -7,4 +7,20 @@ typedef struct {
     char telefone[15];
 } Contato;
 
+typedef struct {
+    Contato contato;
+    int ocupado;
+} EntradaHash;
 
+typedef struct {
+    EntradaHash tabela[TAMANHO_TABELA];
+} TabelaHash;
+
+void inicializar_tabela(TabelaHash *tabela);
+int contato_existe(TabelaHash tabela, Contato contato);
+void inserir_contato(TabelaHash *tabela, Contato contato);
+Contato buscar_contato(TabelaHash tabela, char *chave);
+void listar_contatos(TabelaHash tabela);
+void remover_contato(TabelaHash *tabela, char *nome);
+
+#endif

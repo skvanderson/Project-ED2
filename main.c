f2bf72020f3b9a5be6541021be8f30d04d43117e
Sharlles Andersonn <54425710+skvanderson@users.noreply.github.com>
2024-03-27 13:12:50 -0300

    Update main.c
    Finalizado o menu de opções ao usuário, incluindo adicionar um novo contato, buscar um contato existente, remover um contato, listar todos os contatos e sair do programa. Após isto ele vai entra em um loop infinito, permitindo que o usuário realize várias operações até optar por sair. Cada opção do menu é tratada separadamente, realizando ações como inserção, busca e remoção de contatos na tabela hash.

--- main.c
+++ main.c
@@ -9,3 +9,75 @@ void limpar_tela() {
     system("clear");
 }
 
+int main() {
+    TabelaHash tabela;
+    Contato novo_contato;
+    int opcao;
+    char nome_busca[50], nome_remover[50], opcao_str[50];
+
+    for (int i = 0; i < TAMANHO_TABELA; i++) {
+        tabela.tabela[i].ocupado = 0;
+    }
+    srand(time(NULL));
+
+    while (1) {
+        printf("\nBem-Vindo à Lista Telefônica\n\n");
+        printf("1-Novo Contato\n");
+        printf("2-Buscar Contato\n");
+        printf("3-Remover Contato\n");
+        printf("4-Sair\n");
+        printf("5-Listar Todos os Contatos\n");
+        printf("Escolha uma opção: ");
+        scanf("%s", opcao_str);
+
+        limpar_tela();
+
+        int is_numeric = 1;
+        for (int i = 0; opcao_str[i] != '\0'; i++) {
+            if (!isdigit(opcao_str[i])) {
+                is_numeric = 0;
+                break;
+            }
+        }
+
+        if (!is_numeric) {
+            printf("Opção inválida. Por favor, digite um número.\n");
+            continue;
+        }
+        opcao = atoi(opcao_str);
+
+        switch (opcao) {
+            case 1:
+                printf("Qual é o nome do novo contato?\n");
+                scanf("%s", novo_contato.nome);
+                printf("Qual é o número deste contato?\n");
+                scanf("%s", novo_contato.telefone);
+                inserir_contato(&tabela, novo_contato);
+                break;
+            case 2:
+                printf("Qual contato deseja buscar? (Digite o nome ou número de telefone)\n");
+                scanf("%s", nome_busca);
+                novo_contato = buscar_contato(tabela, nome_busca);
+                if (strcmp(novo_contato.nome, "") != 0) {
+                    printf("Informações do Contato:\nNome: %s\nTelefone: %s\n", novo_contato.nome, novo_contato.telefone);
+                } else {
+                    printf("Contato inexistente.\n");
+                }
+                break;
+            case 3:
+                printf("Digite o nome do contato a ser removido: ");
+                scanf("%s", nome_remover);
+                remover_contato(&tabela, nome_remover);
+                break;
+            case 4:
+                printf("Até mais!\n");
+                exit(0);
+            case 5:
+                listar_contatos(tabela);
+                break;
+            default:
+                printf("Opção inválida. Tente novamente.\n");
+        }
+    }
+    return 0;
+}
